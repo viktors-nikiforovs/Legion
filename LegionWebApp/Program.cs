c9f876a645286dd3a060a13cd8889d13aae88a9d
Viktors Nikiforovs <Viktors.Nikiforovs@outlook.com>
2022-06-13 16:46:32 +0100

    CLEARDB_DATABASE_URL

--- LegionWebApp/Program.cs
+++ LegionWebApp/Program.cs
@@ -12,8 +12,45 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-  options.UseMySql(connectionString, new MySqlServerVersion(new Version(5,6,50)), options => options.EnableRetryOnFailure()));
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//  options.UseMySql(connectionString, new MySqlServerVersion(new Version(5,6,50)), options => options.EnableRetryOnFailure()));
+
+builder.Services.AddDbContext<DbContext>(x =>
+{
+
+    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    string connStr;
+
+    if (env == "Development")
+    {
+        connStr = builder.Configuration.GetConnectionString("DefaultConnection");
+
+
+    }
+    else
+    {
+        // Use connection string provided at runtime by Heroku.
+        var connUrl = Environment.GetEnvironmentVariable("CLEARDB_DATABASE_URL");
+
+        connUrl = connUrl.Replace("mysql://", string.Empty);
+        var userPassSide = connUrl.Split("@")[0];
+        var hostSide = connUrl.Split("@")[1];
+
+        var connUser = userPassSide.Split(":")[0];
+        var connPass = userPassSide.Split(":")[1];
+        var connHost = hostSide.Split("/")[0];
+        var connDb = hostSide.Split("/")[1].Split("?")[0];
+
+
+        connStr = $"server={connHost};Uid={connUser};Pwd={connPass};Database={connDb}";
+
+
+
+    }
+
+    x.UseMySql(connStr, new MySqlServerVersion(new Version(5, 6, 50)));
+
+});
 
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
