efacd887d90d121edfb9f96de134209253e61712
Viktors Nikiforovs <Viktors.Nikiforovs@outlook.com>
2022-10-18 18:26:17 +0100

    Beta

---
+++ LegionWebApp/Models/Gallery.cs
@@ -0,0 +1,11 @@
+﻿namespace LegionWebApp.Models
+{
+    public class Gallery
+    {
+        public string Size { get; set; }
+        public string[] Image { get; set; }
+        public string[] Video { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
