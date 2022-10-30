5901cf284a21da39a85814bfe3f6fcc1620206a8
Viktors Nikiforovs <Viktors.Nikiforovs@outlook.com>
2022-10-30 02:46:00 +0000

    .

--- LegionWebApp/Models/Gallery.cs
+++ LegionWebApp/Models/Gallery.cs
@@ -1,31 +1,37 @@
-﻿namespace LegionWebApp.Models
+﻿using System.Collections;
+
+namespace LegionWebApp.Models
 {
     public class GalleryItem
     {
-        public GalleryItem(string name)
-        {
-            Video = new List<Video>();
-            Name = name;
-        }
-        public string Name { get; set; }
+        public GalleryItem()
+		{
+            ImgSize = 4;
+		}
         public string Title { get; set; }
-        public int Column { get; set; }
-        public string[] Image { get; set; }
-        public List<Video> Video { get; set; }
-        public string Poster { get; set; }
-        public bool HasText { get; set; }
-        public bool SeperateVideo { get; set; }
+        public int ImgSize { get; set; }
+        public Image[] Image { get; set; }
+        public Video[] Video { get; set; }
 
+    }
+    public class Image
+	{
+        public Image(string link)
+		{
+            this.Link = link;
+        }
+        public string Link { get; set; }
     }
     public class Video
     {
-        public Video(string link, string poster = null)
-        {
-            this.Link = link;
-            this.Poster = poster;
-        }
-        public string Link { get; set; }
+        public Video(string link, int size = 4, string poster = null)
+		{
+			this.Link = link;
+			this.Poster = poster;
+            this.Size = size;
+		}
+		public string Link { get; set; }
         public string Poster { get; set; }
-        public int SeperateId { get; set; }
+        public int Size { get; set; }
     }
 }
