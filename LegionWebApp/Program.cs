700811a7794fe4bdd38255ab5f4bffc3f6ae3aef
Viktors Nikiforovs <Viktors.Nikiforovs@outlook.com>
2023-03-05 05:52:56 +0000

    .

--- LegionWebApp/Program.cs
+++ LegionWebApp/Program.cs
@@ -81,7 +81,7 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 var botClient = app.Services.GetRequiredService<ITelegramBotClient>();
-var webhookUrl = "https://beta.legion-foundation.org/Home/Post";
+var webhookUrl = "https://beta.legion-foundation.org/Home/";
 var webhookInfo = await botClient.GetWebhookInfoAsync();
 if (!string.IsNullOrEmpty(webhookInfo.Url))
 {
