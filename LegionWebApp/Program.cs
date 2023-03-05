18b132a4f3baeffa52a602fbef32403fde2311fd
Viktors Nikiforovs <Viktors.Nikiforovs@outlook.com>
2023-03-05 11:03:53 +0000

    Testing webhook

--- LegionWebApp/Program.cs
+++ LegionWebApp/Program.cs
@@ -1,88 +1,61 @@
-using LegionWebApp.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.Mvc.Razor;
-using Microsoft.EntityFrameworkCore;
-using System.Globalization;
 using Telegram.Bot;
-using Telegram.Bot.Types.Enums;
+using LegionWebApp.Controllers;
+using Telegram.Bot.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorPages().AddViewLocalization();
-builder.Services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
-builder.Services.AddPortableObjectLocalization(options => options.ResourcesPath = "Localization");
+// Setup Bot configuration
+var botConfigurationSection = builder.Configuration.GetSection(BotConfiguration.Configuration);
+builder.Services.Configure<BotConfiguration>(botConfigurationSection);
 
-builder.Services.AddSingleton<ITelegramBotClient>(new TelegramBotClient(Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN")));
+var botConfiguration = botConfigurationSection.Get<BotConfiguration>();
 
-builder.Services.Configure<RequestLocalizationOptions>(options =>
-{
-    var supportedCultures = new List<CultureInfo>
-    {
-        new CultureInfo("en-GB"),
-        new CultureInfo("uk"),
-        new CultureInfo("fr"),
-        new CultureInfo("de")
-    };
-    options.DefaultRequestCulture = new RequestCulture("en-GB");
-    options.SupportedCultures = supportedCultures;
-    options.SupportedUICultures = supportedCultures;
-});
+// Register named HttpClient to get benefits of IHttpClientFactory
+// and consume it with ITelegramBotClient typed client.
+// More read:
+//  https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-requests#typed-clients
+//  https://docs.microsoft.com/en-us/dotnet/architecture/microservices/implement-resilient-applications/use-httpclientfactory-to-implement-resilient-http-requests
+builder.Services.AddHttpClient("telegram_bot_client")
+                .AddTypedClient<ITelegramBotClient>((httpClient, sp) =>
+                {
+                    BotConfiguration? botConfig = sp.GetConfiguration<BotConfiguration>();
+                    TelegramBotClientOptions options = new(botConfig.BotToken);
+                    return new TelegramBotClient(options, httpClient);
+                });
 
-var connectionString = builder.Configuration.GetConnectionString("herokypostgresql");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-  options.UseNpgsql(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter(); // Use Migrations
+// Dummy business-logic service
+builder.Services.AddScoped<UpdateHandlers>();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options =>
-{
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequiredLength = 6;
-    options.SignIn.RequireConfirmedAccount = true;
-})
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+// There are several strategies for completing asynchronous tasks during startup.
+// Some of them could be found in this article https://andrewlock.net/running-async-tasks-on-app-startup-in-asp-net-core-part-1/
+// We are going to use IHostedService to add and later remove Webhook
+builder.Services.AddHostedService<ConfigureWebhook>();
 
-// Add console logging
-builder.Logging.AddConsole();
+// The Telegram.Bot library heavily depends on Newtonsoft.Json library to deserialize
+// incoming webhook updates and send serialized responses back.
+// Read more about adding Newtonsoft.Json to ASP.NET Core pipeline:
+//   https://docs.microsoft.com/en-us/aspnet/core/web-api/advanced/formatting?view=aspnetcore-6.0#add-newtonsoftjson-based-json-format-support
+builder.Services
+    .AddControllers()
+    .AddNewtonsoftJson();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-app.UseRequestLocalization();
-
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapRazorPages();
-
-var botClient = app.Services.GetRequiredService<ITelegramBotClient>();
-var webhookUrl = "https://beta.legion-foundation.org/Home/";
-var webhookInfo = await botClient.GetWebhookInfoAsync();
-
-await botClient.SetWebhookAsync(webhookUrl);
+// Construct webhook route from the Route configuration parameter
+// It is expected that BotController has single method accepting Update
+app.MapBotWebhookRoute<BotController>(route: botConfiguration.Route);
+app.MapControllers();
 app.Run();
+
+#pragma warning disable CA1050 // Declare types in namespaces
+#pragma warning disable RCS1110 // Declare type inside namespace.
+public class BotConfiguration
+#pragma warning restore RCS1110 // Declare type inside namespace.
+#pragma warning restore CA1050 // Declare types in namespaces
+{
+    public static readonly string Configuration = "BotConfiguration";
+
+    public string BotToken { get; init; } = default!;
+    public string HostAddress { get; init; } = default!;
+    public string Route { get; init; } = default!;
+    public string SecretToken { get; init; } = default!;
+}
