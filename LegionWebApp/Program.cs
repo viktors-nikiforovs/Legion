4618ed7c9a3b5300b53de9e990d8ec8586a1b7e6
Viktors Nikiforovs <Viktors.Nikiforovs@outlook.com>
2023-04-08 12:17:31 +0100

    .

--- LegionWebApp/Program.cs
+++ LegionWebApp/Program.cs
@@ -118,7 +118,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Admin}/{action=GalleryCreate}/{id?}");
 
 
 app.MapRazorPages();
